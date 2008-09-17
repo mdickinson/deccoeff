9775eb3f708d75ed1b0066accbaec28b8a75673d
dickinsm <dickinsm@gmail.com>
2008-09-17 14:11:08 +0100

    snip unnecessary stuff from run.py
    darcs-hash:20080917131108-224ad-431a1103a74aa693269d01d5854782adf88911d8

--- run.py
+++ run.py
@@ -1,27 +1,3 @@
 import decimal
 D = decimal.Decimal
 DI = decimal.Deccoeff
-
-# test divmod (and also, incidentally, multiplication and addition)
-from random import randrange, choice
-
-def test_divmod():
-    # random lengths for deccoeffs
-    alen = randrange(10)
-    blen = randrange(10)
-
-    a = DI(''.join(choice('0123456789') for i in range(alen)))
-    b = DI(''.join(choice('0123456789') for i in range(blen)))
-    if (not b):
-        return
-    q, r = divmod(a, b)
-    assert b*q+r == a and 0 <= r < b
-
-    # also check that results are same as for longs
-    qq, rr = divmod(int(str(a) or '0'), int(str(b) or '0'))
-    assert qq == int(str(q) or '0') and rr == int(str(r) or '0')
-
-
-
-x = DI('9000000000000000')
-print(x << 3)
